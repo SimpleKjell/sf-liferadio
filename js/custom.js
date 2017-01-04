c1b5baef493d6792d8d1a21fc20ac95448e7dc60
Kjell <kjell@simplefox.de>
2017-01-04 19:11:58 +0100

    liferadio final

--- js/custom.js
+++ js/custom.js
@@ -1,125 +1,226 @@
 jQuery(document).ready(function(jQuery)
 {
-
-
 	sfl_frontend = new SFLFrontEnd();
-
-
-
 });
 
 function SFLFrontEnd() {
-  //this.init();
+  this.init();
 };
 
-PRBFrontEnd.prototype = {
+SFLFrontEnd.prototype = {
 	/* Alles wichtige was am Anfang gebraucht wird */
 	init:function () {
 		/* Aufrufen von wichtigen Funktionen */
+		this.fbInit();
 		this.formSubmit();
-		this.goalProgress();
+		this.svgInit();
+		this.wallInit();
 	},
-	validateParsleyForm: function() {
+	wallInit: function() {
 
-		jQuery('.prb_form').parsley();
+		// Bereche, wieviele nebeneinander können
+		var window_width = jQuery('.sfl_wall_sub_container').width();
+		var wall_container_width = window_width * 2/3;
+
+		jQuery('.sfl_wall_sub_container_inner').css('width', wall_container_width);
+
+		var numItems = jQuery('.sfl_wall_sub_single').length
+		var itemWidth = jQuery('.sfl_wall_sub_single').outerWidth();
+		var howManyToShowSlide = Math.floor(wall_container_width / itemWidth);
+		console.log(itemWidth);
+		console.log(howManyToShowSlide);
+		if(numItems > howManyToShowSlide) {
+			jQuery('.sfl_wall_sub_container_inner_block').slick({
+
+				centerMode: true,
+			  centerPadding: '60px',
+			  slidesToShow: 3,				
+				variableWidth: true,
+				arrows: false
+			});
+			/*
+			infinite: true,
+			slidesToShow: 3,
+			slidesToScroll: 3,
+			variableWidth: true,
+			centerMode: true,
+			adaptiveHeight: true,
+			autoplay: true,
+			autoplaySpeed: 4000,
+			arrows: false
+			*/
+
+			jQuery('.sfl_nav_right').click(function() {
+					var currentSlide = parseInt(jQuery('.slick-current').attr('data-slick-index'));
+					jQuery('.sfl_wall_sub_container_inner_block').slick('slickGoTo',currentSlide+1);
+			})
+
+			jQuery('.sfl_nav_left').click(function() {
+					var currentSlide = parseInt(jQuery('.slick-current').attr('data-slick-index'));
+					if(currentSlide != '0') {
+							jQuery('.sfl_wall_sub_container_inner_block').slick('slickGoTo',currentSlide-1);
+					}
+			})
+		} else {
+			// Es sind nicht genügend Slides vorhanden, also blende die Pfeile aus
+			jQuery('.sfl_nav_left').hide();
+			jQuery('.sfl_nav_right').hide();
+		}
+
+
+
 	},
-	goalProgress: function() {
+	fbInit: function() {
 
-		// Bundesländer anzeigen
-		jQuery('.showBundeslandGoals').click(function(evt) {
-			evt.preventDefault();
-			jQuery('.bundesland_goals').fadeIn(1000, function() {
-					jQuery('.hideBundeslandGoals').show();
-			});
-			jQuery(this).hide();
-
-		})
-
-		// Bundesländer verbergen
-		jQuery('.hideBundeslandGoals').click(function(evt) {
-			evt.preventDefault();
-			jQuery('.bundesland_goals').fadeOut(700, function() {
-					jQuery('.showBundeslandGoals').show();
-			});
-			jQuery(this).hide();
-
-		})
-
-		var main_goal_value = jQuery('#main_goal').attr('data-value');
-		var goalAmount = parseInt(main_goal_value) *100 / 80;
-
-		jQuery('#main_goal').goalProgress({
-        goalAmount: goalAmount,
-        currentAmount: main_goal_value,
-        textBefore: '€',
-        textAfter: ' gesammelt.'
-    });
-
-		jQuery('.goals').each(function() {
-
-			var goal_value = parseInt(jQuery(this).attr('data-value'));
-
-			if(goal_value == 0) {
-				goalAmount = 10000;
-			} else if (goal_value < 3) {
-				goalAmount = 10000;
-			} else {
-				goalAmount = parseInt(goal_value) *100 / 80;
-			}
-
-
-
-			jQuery(this).goalProgress({
-	        goalAmount: goalAmount,
-	        currentAmount: goal_value,
-	        textBefore: '€',
-	        textAfter: ' gesammelt.'
+		window.fbAsyncInit = function() {
+	    FB.init({
+	      appId      : '365222543851422',
+	      xfbml      : true,
+	      version    : 'v2.7'
 	    });
+	  };
+
+	  (function(d, s, id){
+	     var js, fjs = d.getElementsByTagName(s)[0];
+	     if (d.getElementById(id)) {return;}
+	     js = d.createElement(s); js.id = id;
+	     js.src = "//connect.facebook.net/en_US/sdk.js";
+	     fjs.parentNode.insertBefore(js, fjs);
+	   }(document, 'script', 'facebook-jssdk'));
+
+	},
+	svgInit: function() {
+
+
+		// Schreibe aus dem Textfeld den Value in das SVG Element Text
+		jQuery('#wunsch').keyup(function() {
+			var value = jQuery(this).val();
+			value = value.toUpperCase();
+			jQuery('#fb_image_text').text(value);
+			jQuery('#fb_image_text_wall').text(value);
 		})
 
+		// Schreibe den Namen auf das Wall Bild
+		jQuery('#vorname').keyup(function() {
+			var value = jQuery(this).val();
+			jQuery('#wall_image_name').text(value);
+		})
 
 
 	},
 	formSubmit: function() {
-		jQuery('.prb_form').submit(function(evt) {
+
+
+
+		jQuery('#sfl_form_container').submit(function(evt) {
+
+			// Stop form from submitting normally
+  		evt.preventDefault();
 
 			// Validierung hier
-			if(jQuery('.prb_form').parsley().validate()) {
-				var vorname = jQuery('.prb_form #vorname').val();
-				var nachname = jQuery('.prb_form #nachname').val();
-				var tel = jQuery('.prb_form #tel').val();
-				var mail = jQuery('.prb_form #mail').val();
-				var bundesland = jQuery('.prb_form #bundesland').val();
-				var nonce = jQuery('.prb_form #mail_nonce').val();
+			if(jQuery('#sfl_form_container').parsley().validate()) {
 
-				var sendMails = jQuery.ajax({
-						url : Custom.ajaxurl,
-						type : 'post',
-						data : {
-							 action: 'send_prb_mails',
-							 vorname: vorname,
-							 nachname: nachname,
-							 bundesland: bundesland,
-							 tel: tel,
-							 mail: mail,
-							 nonce : nonce
-						},
+				var vorname = jQuery('#sfl_form_container #vorname').val();
+				var nachname = jQuery('#sfl_form_container #nachname').val();
+				var tel = jQuery('#sfl_form_container #telefon').val();
+				var mail = jQuery('#sfl_form_container #email').val();
+				var nonce = jQuery('#sfl_form_container #form_nonce').val();
+
+
+
+				// Canvas erstellen FB Teilung
+				// Canvas Element FB Teilung
+				var c=document.getElementById("svg-canvas");
+				var html = jQuery('#mySVG').html();
+				html = html.replace(/>\s+/g, ">").replace(/\s+</g, "<");
+				canvg(c, '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1200" height="627">'+html+'</svg>', { ignoreMouse: true, ignoreAnimation: true });
+
+				// Canvas erstellen Wall
+				// Canvas Element Wall
+				var c=document.getElementById("svg-canvas-wall");
+				var html = jQuery('#mySVGWall').html();
+				html = html.replace(/>\s+/g, ">").replace(/\s+</g, "<");
+				canvg(c, '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="250" height="250">'+html+'</svg>', { ignoreMouse: true, ignoreAnimation: true });
+
+
+				data = {
+					 action: 'add_new_suberino',
+					 nonce : nonce,
+					 vorname : vorname,
+					 nachname : nachname,
+					 telefon : tel,
+					 mail : mail
+				}
+
+				var addNewSuberino = jQuery.ajax(
+					Custom.ajaxurl,{
+						data,
+						type : "POST",
 						beforeSend: function( xhr ) {
-							jQuery('.prb_form_container').append('<div class="xhrWait"><center><i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i><br /><br />Deine Anfrage wird bearbeitet. Bitte habe ein wenig Geduld.</center></div>');
-							jQuery('.prb_form').hide();
-							//jQuery(that).parent().append('<center><i style="font-size: 14px;color:#EA4E92;" class="fa fa-spinner fa-pulse fa-3x fa-fw"></i> wird bearbeitet</center>');
-							//jQuery('.edit, .close, .delete').remove();
-						},
-						error : function(error) {
-							console.log(error)
-							alert('Keine Berechtigung')
-						}
-				})
 
-				sendMails.done(function(response) {
-					jQuery('.prb_form').hide();
-					jQuery('.xhrWait').remove();
-					jQuery('.prb_form_abschlusscreen').show();
+							jQuery('#sfl_form_container').hide();
+							jQuery('.sfl_container').append('<div class="xhrWait sfl_box sfl_flow_text"><center><i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i><br /><br />Dein Wunsch wird bearbeitet. Bitte habe ein wenig Geduld.</center></div>');
+
+						},
+					}
+				);
+
+				addNewSuberino.done(function(response) {
+					console.log(response);
+
+
+					// Jetzt das jpg speichern und dem User zuordnen
+					// FB Teilung
+					var c=document.getElementById("svg-canvas");
+					var img_src =  c.toDataURL('image/png')
+					jQuery('#svg-img').attr('src', img_src);
+
+					// Wall
+					var c=document.getElementById("svg-canvas-wall");
+					var img_src_wall =  c.toDataURL('image/png')
+					jQuery('#svg-img-wall').attr('src', img_src_wall);
+
+					var teilnehmer_id = response;
+					jQuery.ajax({
+							url : Custom.ajaxurl,
+							type : 'post',
+							data : {
+								 action : 'add_design_to_sub',
+								 teilnehmer_id : teilnehmer_id,
+								 img_src : img_src,
+								 img_src_wall : img_src_wall
+							},
+							beforeSend: function( xhr ) {
+							},
+							error : function(error) {
+								console.log(error)
+								alert('Keine Berechtigung')
+							},
+							success: function(uri) {
+								console.log(uri);
+								jQuery('.xhrWait').hide();
+								jQuery('.finish_process_sub_teilnahme').css('display', 'inline-block');
+
+
+
+								// FB Share
+								jQuery("#sfl_fb_share").click(function(a) {
+			            a.preventDefault(), FB.ui({
+			                method: "feed",
+											name: "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem",
+			                link: "http://google.at/",
+											picture: uri,
+			                caption: "Liferadio verdoppelt dein Gehalt!",
+			                description: "Veroppel dein Gehalt! Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum",
+			            }, function(a) {})
+
+									jQuery('.finish_process_sub_teilnahme').hide();
+									jQuery('.finished_sub_teilnahme').css('display', 'inline-block');
+								})
+
+							}
+					})
+
 				})
 			}
 
--- sflclasses/sfl.sfliferadio.shortcodes.php
+++ sflclasses/sfl.sfliferadio.shortcodes.php
@@ -9,10 +9,17 @@ class SFLshortCode {
 		add_action( 'init',   array(&$this,'sfliferadio_shortcodes'));
 
     // Our custom post type function
-    //add_action( 'init', array(&$this, 'create_custom_post_type' ));
+    add_action( 'init', array(&$this, 'create_custom_post_type' ));
 
     // Custom Meta Box
-    //add_action( 'add_meta_boxes', array(&$this, 'create_custom_meta_box' ));
+    add_action( 'add_meta_boxes', array(&$this, 'create_custom_meta_box' ));
+
+    add_action('wp_ajax_add_new_suberino', array( $this, 'add_new_suberino' ));
+    add_action('wp_ajax_nopriv_add_new_suberino', array( $this, 'add_new_suberino' ));
+
+    add_action('wp_ajax_add_design_to_sub', array( $this, 'add_design_to_sub' ));
+    add_action('wp_ajax_nopriv_add_design_to_sub', array( $this, 'add_design_to_sub' ));
+
 
     /*
     * Global Options
@@ -30,7 +37,7 @@ class SFLshortCode {
       'custom-sub-box',      // Unique ID
       'Teilnehmer Informationen',    // Title
       array(&$this, 'sub_meta_box' ),   // Callback function
-      'anfragen',         // Admin page (or post type)
+      'teilnehmer',         // Admin page (or post type)
       'normal',         // Context
       'default'         // Priority
     );
@@ -63,17 +70,17 @@ class SFLshortCode {
   * Gewinnspielteilnehmer
   */
   function create_custom_post_type() {
-    register_post_type( 'anfragen',
+    register_post_type( 'teilnehmer',
     // CPT Options
         array(
             'labels' => array(
-                'name' => 'Anfragen',
+                'name' => 'Teilnehmer',
             ),
             'public'    => false,
             'show_ui'            => true,
 		        'show_in_menu'       => true,
             'has_archive' => false,
-            'rewrite' => array('slug' => 'anfragen'),
+            'rewrite' => array('slug' => 'teilnehmer'),
             'supports' => array('title','thumbnail')
         )
     );
@@ -84,25 +91,27 @@ class SFLshortCode {
   */
   function showSubInformation($field, $value, $subID) {
 
+
     switch($field) {
-      case 'Vorname':
-        $label = $field;
+      case 'vorname':
+        $label = "Vorname";
         $value = $value[0];
         break;
-      case 'Nachname':
-        $label = $field;
+      case 'nachname':
+        $label = "Nachname";
         $value = $value[0];
         break;
-      case 'E-Mail':
-        $label = $field;
+      case 'email':
+        $label = "E-Mail";
         $value = $value[0];
         break;
-      case 'Telefon':
-        $label = $field;
+      case 'phone':
+        $label = "Telefon";
         $value = $value[0];
-      case 'Bundesland':
-        $label = $field;
-        $value = $value[0];
+        break;
+      case 'sfl_wall_picture':
+        $label = "Wall Picture";
+        $value = '<img style="margin-top:25px;" src="'.$value[0].'" />';
         break;
       default:
         $label = '';
@@ -134,33 +143,94 @@ class SFLshortCode {
     // Frontend Form
     add_shortcode( 'sfliferadio_form', array(&$this,'sfliferadio_form_function') );
 
+    // Frontend Wall
+    add_shortcode( 'sfliferadio_wall', array(&$this,'sfliferadio_wall_function') );
+
 	}
+
+  public function sfliferadio_wall_function($atts)
+  {
+
+    // Alle Teilnehmer
+    $args = array(
+    	'posts_per_page'   => -1,
+    	'offset'           => 0,
+    	'category'         => '',
+    	'category_name'    => '',
+    	'orderby'          => 'date',
+    	'order'            => 'DESC',
+    	'include'          => '',
+    	'exclude'          => '',
+    	'meta_key'         => '',
+    	'meta_value'       => '',
+    	'post_type'        => 'teilnehmer',
+    	'post_mime_type'   => '',
+    	'post_parent'      => '',
+    	'author'	   => '',
+    	'author_name'	   => '',
+    	'post_status'      => 'publish',
+    	'suppress_filters' => true
+    );
+    $posts_array = get_posts( $args );
+
+    ob_start();
+    ?>
+    <div class="sfl_wall_sub_container">
+      <center>
+        <div class="sfl_nav_left">
+          <img width="50px" src="<?php echo sfliferadio_url . "templates/".sfliferadio_template. "/img/pfeillinks.png"; ?>" />
+        </div>
+        <div class="sfl_wall_sub_container_inner">
+          <div class="sfl_wall_sub_container_inner_block">
+            <?php
+            foreach ($posts_array as $teilnehmer) {
+
+              ?>
+              <div class="sfl_wall_sub_single">
+                <img width="150" src="<?php echo $teilnehmer->sfl_wall_picture;?>" />
+              </div>
+              <?php
+            }
+            ?>
+          </div>
+        </div>
+        <div class="sfl_nav_right">
+          <img width="50px" src="<?php echo sfliferadio_url . "templates/".sfliferadio_template. "/img/pfeilrechts.png"; ?>" />
+        </div>
+      </center>
+    </div>
+    <?php
+    //assign the file output to $content variable and clean buffer
+		$content = ob_get_clean();
+		return  $content;
+  }
 
   public function sfliferadio_form_function($atts)
   {
     ob_start();
     ?>
+    <script type="text/javascript" src="http://canvg.github.io/canvg/canvg.js"></script>
     <div class="sfl_container">
-      <form>
+      <form id="sfl_form_container" method="POST">
         <div class="sfl_container_data sfl_box">
           <div class="sfl_input_field">
             <label>Vorname</label>
-            <input type="text" name="vorname" id="vorname" />
+            <input type="text" name="vorname" id="vorname" maxlength="15" required="" />
           </div>
           <div class="clear"></div>
           <div class="sfl_input_field">
             <label>Nachname</label>
-            <input type="text" name="nachname" id="nachname" />
+            <input type="text" name="nachname" id="nachname" required=""/>
           </div>
           <div class="clear"></div>
           <div class="sfl_input_field">
             <label>Telefon</label>
-            <input type="text" name="telefon" id="telefon" />
+            <input type="text" name="telefon" id="telefon" required=""/>
           </div>
           <div class="clear"></div>
           <div class="sfl_input_field">
             <label>E-Mail</label>
-            <input type="email" name="email" id="email" />
+            <input type="email" name="email" id="email" required=""/>
           </div>
           <div class="clear"></div>
         </div>
@@ -170,15 +240,193 @@ class SFLshortCode {
           </div>
           <div class="sfl_wunsch_input center">
             = 1 x
-            <input type="text" name="wunsch" id="wunsch" />
+            <input type="text" name="wunsch" id="wunsch" maxlength="14" required=""/>
           </div>
+          <?php $nonce = wp_create_nonce( 'user-submit-form' ); ?>
+          <input type="hidden" id="form_nonce" value="<?php echo $nonce;?>">
+          <input type="submit" value="weiter" />
         </div>
       </form>
+      <div class="svg_edit_container">
+        <svg id="mySVG" version="1.1"  xmlns="http://www.w3.org/2000/svg" class="designer_svg" width="1200" height="627" viewBox="0 0 1200 627" preserveAspectRatio="xMidYMid meet">
+            <g class="image-layer">
+              <image id="default_fb_image" xlink:href="<?php echo sfliferadio_url . "templates/".sfliferadio_template. "/img/userpost_default.jpg"; ?>" width="1200" height="627" x="0" y="0"/>
+            </g>
+            <g class="text-layer">
+              <text x="650" y="378" font-family="XXMarker" id="fb_image_text" font-size="75">
+
+              </text>
+            </g>
+        </svg>
+        <svg id="mySVGWall" version="1.1"  xmlns="http://www.w3.org/2000/svg" class="designer_svg" width="250" height="250" viewBox="0 0 250 250" preserveAspectRatio="xMidYMid meet">
+            <g class="image-layer">
+              <image id="default_fb_image" xlink:href="<?php echo sfliferadio_url . "templates/".sfliferadio_template. "/img/wall_default_ohneborder.jpg"; ?>" width="250" height="250" x="0" y="0"/>
+            </g>
+            <g class="text-layer">
+              <text text-anchor="middle" x="125" y="50" font-family="Rams" id="wall_image_name" font-size="30">
+              </text>
+
+              <text x="95" y="165" font-family="XXMarker" id="fb_image_text_wall" font-size="20">
+              </text>
+            </g>
+        </svg>
+        <canvas id="svg-canvas" width=1200 height=627></canvas>
+        <canvas id="svg-canvas-wall" width=1200 height=627></canvas>
+        <img id="svg-img" />
+        <img id="svg-img-wall" />
+      </div>
+
+      <div class="finish_process_sub_teilnahme sfl_box">
+        <div class="sfl_heading center marginBottomMedium">
+          Du hast es fast geschafft!
+        </div>
+        <div class="sfl_flow_text center marginBottomMedium">
+          Du musst nur noch deinen Wunsch auf Facebook teilen,
+          und du nimmst am Gewinnspiel teil.
+        </div>
+        <center>
+          <button id="sfl_fb_share" >Teilen & Mitmachen </button>
+        </center>
+      </div>
+      <div class="finished_sub_teilnahme sfl_box">
+        <div class="sfl_heading center marginBottomMedium">
+          Viel Glück!
+        </div>
+        <div class="sfl_flow_text center marginBottomMedium">
+          Dein Wunsch ist eingetragen. <br/>
+          Du wirst benachrichtigt, sobald du als Gewinner gezogen wurdest. <br />
+          Die Gewinner werden nur unter denjenigen gezogen, die ihren Wunsch auf Facebook geteilt haben.
+        </div>
+      </div>
     </div>
     <?php
     //assign the file output to $content variable and clean buffer
 		$content = ob_get_clean();
 		return  $content;
+  }
+
+
+  /*
+  * Add New Subscriper
+  * Neuen Teilnehmer hinzufügen
+  * Jeweils neuen Eintrag Custom Post Type Teilnehmer
+  */
+  function add_new_suberino()
+  {
+
+    if ( !wp_verify_nonce( $_REQUEST['nonce'], "user-submit-form")) {
+      // TODO ERRORE zurücksenden
+      exit("No naughty business please");
+    }
+
+    $vorname = $_REQUEST['vorname'];
+    $nachname = $_REQUEST['nachname'];
+    $telefon = $_REQUEST['telefon'];
+    $email = $_REQUEST['mail'];
+
+
+
+
+
+    // Neuen Teilnehmer hinzufügen
+    $new_sub = array(
+      'post_title'    => wp_strip_all_tags( $vorname ),
+      'post_status'   => 'publish',
+      'post_type'     => 'teilnehmer',
+      'post_author'   => 1,
+    );
+
+    // Teilnehmer hinzufügen
+    $teilnehmer_id = wp_insert_post( $new_sub );
+
+    // Alle Daten dem Teilnehmer zuordnen
+    if($teilnehmer_id !== 0) {
+      update_post_meta($teilnehmer_id, "vorname", $vorname);
+      update_post_meta($teilnehmer_id, "nachname", $nachname);
+      update_post_meta($teilnehmer_id, "email", $email);
+      update_post_meta($teilnehmer_id, "phone", $telefon);
+
+      echo $teilnehmer_id;
+    }
+    die();
+  }
+
+  public function add_design_to_sub()
+  {
+    $teilnehmer = $_REQUEST['teilnehmer_id'];
+    $imageSrc = $_REQUEST['img_src'];
+    $imageSrcWall = $_REQUEST['img_src_wall'];
+
+    $data = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $imageSrc));
+
+    $wp_upload_dir = wp_upload_dir();
+    $upDir = wp_upload_dir();
+
+
+
+    file_put_contents($upDir['path'].'/'.$teilnehmer.'.png', $data);
+    $picUrl = $upDir['url'].'/'.$teilnehmer.'.png';
+
+
+    // $filename should be the path to a file in the upload directory.
+    $filename = $upDir['path'].'/'.$teilnehmer.'.png';
+
+    // The ID of the post this attachment is for.
+    $parent_post_id = $teilnehmer;
+
+    // Check the type of file. We'll use this as the 'post_mime_type'.
+    $filetype = wp_check_filetype( basename( $filename ), null );
+
+    // Get the path to the upload directory.
+    $wp_upload_dir = wp_upload_dir();
+
+    // Prepare an array of post data for the attachment.
+    $attachment = array(
+    	'guid'           => $wp_upload_dir['url'] . '/' . basename( $filename ),
+    	'post_mime_type' => $filetype['type'],
+    	'post_title'     => preg_replace( '/\.[^.]+$/', '', basename( $filename ) ),
+    	'post_content'   => '',
+    	'post_status'    => 'inherit'
+    );
+
+    // Insert the attachment.
+    $attach_id = wp_insert_attachment( $attachment, $filename, $parent_post_id );
+
+    // Make sure that this file is included, as wp_generate_attachment_metadata() depends on it.
+    require_once( ABSPATH . 'wp-admin/includes/image.php' );
+
+    // Generate the metadata for the attachment, and update the database record.
+    $attach_data = wp_generate_attachment_metadata( $attach_id, $filename );
+    wp_update_attachment_metadata( $attach_id, $attach_data );
+
+    set_post_thumbnail( $parent_post_id, $attach_id );
+
+    echo $wp_upload_dir['url'] . '/' . basename( $filename );
+
+
+    $this->add_wall_picture_to_sub($imageSrcWall, $teilnehmer);
+
+    die();
+  }
+
+  public function add_wall_picture_to_sub($imageSrc, $teilnehmer)
+  {
+
+    $data = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $imageSrc));
+
+    $wp_upload_dir = wp_upload_dir();
+    $upDir = wp_upload_dir();
+
+    file_put_contents($upDir['path'].'/'.$teilnehmer.'Wall.png', $data);
+    $picUrl = $upDir['url'].'/'.$teilnehmer.'Wall.png';
+
+
+    // $filename should be the path to a file in the upload directory.
+    $fileUrl = $upDir['url'].'/'.$teilnehmer.'Wall.png';
+
+    update_post_meta($teilnehmer, 'sfl_wall_picture', $fileUrl);
+
+
   }
 
 }
